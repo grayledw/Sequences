97e32fea22c9e28690dc539b69cb8289274d6bfc
grayledw <grayledw@rose-hulman.edu>
2017-09-25 17:48:24 -0400

    finished m3 and m4 and m5 working on m6

--- src/m6_fancy_iterating.py
+++ src/m6_fancy_iterating.py
@@ -53,12 +53,12 @@ def main():
                                   sequence3, sequence4)
     run_test_print_items_at_odd_indices(sequence1, sequence2,
                                   sequence3, sequence4)
-    run_test_print_items_in_second_half(sequence1, sequence2,
-                                  sequence3, sequence4)
+    #run_test_print_items_in_second_half(sequence1, sequence2,
+                                       #sequence3, sequence4)
+
+    run_test_print_items_that_are_bigger_than_5()  # Uses different sequences
 #
-#     run_test_print_items_that_are_bigger_than_5()  # Uses different sequences
-#
-#     run_test_print_items_that_are_strings(sequence1, sequence2,
+#    run_test_print_items_that_are_strings(sequence1, sequence2,
 #                                       sequence3, sequence4)
 #     run_test_print_items_that_are_odd_integers(sequence1, sequence2,
 #                                            sequence3, sequence4)
@@ -478,10 +478,13 @@ def print_items_that_are_bigger_than_5(sequence):
     Precondition: All the items in the sequence are integers.
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # Done: 7. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
 
+    for k in range(len(sequence)):
+        if sequence[k] > 5:
+            print(sequence[k], 'is at index', k)
 
 # ----------------------------------------------------------------------
 # Iterating through a sequence, selecting items:
